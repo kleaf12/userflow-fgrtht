26bd158b314ded195bf8aada3df6135a3418cbcb
Grigoriadi <afina.grigoriadi0@mail.ru>
2026-04-18 22:20:07 +0300

    feat: add phone formatter input demo

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,15 +1,12 @@
 import { QueryProvider } from "@app/providers/QueryProvider";
 import { AppRouter } from "@app/router/AppRouter";
-import { formatPhone } from "../utilities/formatPhone";
 
-console.log("Test 1:", formatPhone("89991234567"));
-console.log("Test 2:", formatPhone("+7 (999) 1234567"));
-console.log("Test 3:", formatPhone("7-999-123-45-67"));
-console.log("Test 4:", formatPhone("9991234567"));
-console.log("Test 5:", formatPhone("abcg"));
+import { PhoneFormatterDemo } from "../widgets/PhoneFormatterDemo";
+
 export const App = () => {
   return (
     <QueryProvider>
+      <PhoneFormatterDemo />
       <AppRouter />
     </QueryProvider>
   );
